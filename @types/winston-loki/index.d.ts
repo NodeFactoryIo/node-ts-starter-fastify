976ef9dd13e0afaa15b8dbc39e7afeb558274abd
Marin Petrunić <marin.petrunic@gmail.com>
2020-08-21 09:44:18 +0200

    fix types

--- @types/winston-loki/index.d.ts
+++ @types/winston-loki/index.d.ts
@@ -1,21 +1,21 @@
-import * as Transport from "winston-transport";
-
 declare module "winston-loki" {
 
-     interface LokiTransportOptions {
-        host: string;
-        interval?: number;
-        json?: object;
-        batching?: boolean;
-        clearOnError?: boolean;
-        replaceOnError?: boolean;
-        replaceTimestamp?: boolean;
-        gracefulShutdown?: boolean;
+  import TransportStream from "winston-transport";
 
-    }
+  interface LokiTransportOptions {
+    host: string;
+    interval?: number;
+    json?: object;
+    batching?: boolean;
+    clearOnError?: boolean;
+    replaceOnError?: boolean;
+    replaceTimestamp?: boolean;
+    gracefulShutdown?: boolean;
 
-    export class LokiTransport extends Transport {
-        constructor(options: LokiTransportOptions);
-    }
+  }
+
+  export class LokiTransport extends TransportStream {
+    constructor(options: LokiTransportOptions);
+  }
 }
 
