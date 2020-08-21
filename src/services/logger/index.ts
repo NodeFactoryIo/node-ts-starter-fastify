486ca569175ab1aabcc7817680b456c9ca338ab9
nmlinaric <nikola.mlinaric93@gmail.com>
2020-08-21 10:28:26 +0200

    Add enable loki logging via env

--- src/services/logger/index.ts
+++ src/services/logger/index.ts
@@ -1,5 +1,6 @@
 import winston from "@nodefactory/winston";
 import {LokiTransport} from "winston-loki";
+import Transports from "winston-transport";
 
 const format = winston.format.printf(({ level, message, label, timestamp, requestId }) => {
   message = winston.format.colorize({all: false, message: true}).colorize(level, message);
@@ -10,6 +11,22 @@ const format = winston.format.printf(({ level, message, label, timestamp, reques
   return log;
 });
 
+const transportsConfig: Transports[] = [
+  new winston.transports.Console({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.align(),
+        format
+    )
+  }),
+]
+
+if(process.env.LOKI_LOGGING ?? "") {
+  transportsConfig.push(new LokiTransport({
+    host: "http://localhost:3100",
+  }));
+}
+
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "debug",
   
@@ -17,16 +34,5 @@ export const logger = winston.createLogger({
   defaultMeta: {
     label: "default"
   },
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.align(),
-        format
-      )
-    }),
-    new LokiTransport({
-      host: "http://localhost:3100",
-    })
-  ]
+  transports: transportsConfig
 });
