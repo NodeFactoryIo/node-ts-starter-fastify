dd49c52bb33a6b62ddcad3aa4ac8876df87df554
nmlinaric <nikola.mlinaric93@gmail.com>
2020-08-24 16:01:44 +0200

    Winston loki typings

--- @types/winston-loki/index.d.ts
+++ @types/winston-loki/index.d.ts
@@ -1,20 +1,20 @@
 declare module "winston-loki" {
 
   import TransportStream from "winston-transport";
+  import * as logform from 'logform';
 
   interface LokiTransportOptions {
     host: string;
     interval?: number;
-    json?: object;
+    json?: boolean;
     batching?: boolean;
     clearOnError?: boolean;
-    replaceOnError?: boolean;
     replaceTimestamp?: boolean;
+    labels?: object;
+    format?: logform.Format;
     gracefulShutdown?: boolean;
-
   }
-
-  export class LokiTransport extends TransportStream {
+  export default class LokiTransport extends TransportStream {
     constructor(options: LokiTransportOptions);
   }
 }
