d91c617f5553c560df656edc82257a769fcf8041
nmlinaric <nikola.mlinaric93@gmail.com>
2020-08-28 13:50:42 +0200

    Support js files + lint fix

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,15 +1,15 @@
 const ROOT_DIR = process.env.ROOT_DIR;
 
 export = {
-    entities: `${ROOT_DIR}/entities/*.ts`,
-    migrations: `${ROOT_DIR}/migrations/*.ts`,
-    subscribers: `${ROOT_DIR}/subscribers/*.ts`,
-    seedingFactories: `${ROOT_DIR}/services/db/factories/**/*{.ts}`,
-    seedingSeeds: `${ROOT_DIR}/services/db/seeders/**/*{.ts}`,
-    cli: {
-        entitiesDir: `${ROOT_DIR}/entities`,
-        migrationsDir: `${ROOT_DIR}/migrations`,
-        subscribersDir: `${ROOT_DIR}/subscribers`,
-    },
-    synchronize: process.env.TYPEORM_SYNCHRONIZE
+  entities: `${ROOT_DIR}/entities/*{.ts,.js}`,
+  migrations: `${ROOT_DIR}/migrations/*{.ts,.js}`,
+  subscribers: `${ROOT_DIR}/subscribers/*{.ts,.js}`,
+  seedingFactories: `${ROOT_DIR}/services/db/factories/**/*{.ts,.js}`,
+  seedingSeeds: `${ROOT_DIR}/services/db/seeders/**/*{.ts,.js}`,
+  cli: {
+    entitiesDir: `${ROOT_DIR}/entities`,
+    migrationsDir: `${ROOT_DIR}/migrations`,
+    subscribersDir: `${ROOT_DIR}/subscribers`,
+  },
+  synchronize: process.env.TYPEORM_SYNCHRONIZE
 };
