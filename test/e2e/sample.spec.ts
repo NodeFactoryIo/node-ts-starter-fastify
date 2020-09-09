338775243f22f0c8ef624edf7558310e27bc1f50
nmlinaric <nikola.mlinaric93@gmail.com>
2020-09-09 09:42:23 +0200

    Added e2e sample test

---
+++ test/e2e/sample.spec.ts
@@ -0,0 +1,25 @@
+import {expect} from "chai";
+import {app} from "./app-setup";
+import {logger} from "../../src/services/logger";
+
+describe("Sample e2e test", function () {
+
+  beforeEach(async function () {
+    logger.silent = true;
+  });
+
+  afterEach(async function () {
+    logger.silent = false;
+  });
+
+  it("Should successfully query database", async function () {
+    const res = await app.instance.inject({
+      method: "GET",
+      path: "/samples",
+    });
+
+    expect(res.json()).not.to.be.undefined;
+    expect(res.json()).not.to.be.equal(null);
+    expect(res.json()).to.be.deep.equal([]);
+  });
+});
